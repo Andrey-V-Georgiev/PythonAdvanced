d749d05a7d5b823463755ce4c84a9d21c5e87a2c
Andrey-V-Georgiev <andrey.v.georgiev@gmail.com>
2020-06-12 23:49:03 +0300

    _02_present_delivery(83%)

--- _17_FEB_2020_PREPARATION/_02_present_delivery.py
+++ _17_FEB_2020_PREPARATION/_02_present_delivery.py
@@ -1,125 +1,117 @@
-from collections import deque
-
-# directions
-move_up = (-1, 0)
-move_down = (1, 0)
-move_left = (0, -1)
-move_right = (0, 1)
+directions = {
+    "up": (-1, 0),
+    "right": (0, 1),
+    "down": (1, 0),
+    "left": (0, -1)
+}
 
 santa_position = []
-naughty_kids_count = 0
 nice_kids_count = 0
+count_presents = int(input())
+nice_kids_given_presents = 0
+size_matrix = int(input())
 
-# dimensions input data
-count_of_presents = int(input())
-size_of_matrix = int(input())
 
-# receive the matrix
-matrix = list()
-for row in range(size_of_matrix):
-    row_input_list = list(input().split())
-    matrix.append(row_input_list)
-    for col in range(size_of_matrix):
-        current_cell = row_input_list[col]
+def print_when_not_enough_presents(_nice_kids_with_given_present):
+    nice_kids_left = nice_kids_count - _nice_kids_with_given_present
+    print('Santa ran out of presents!')
+    for current_row in neighborhood:
+        print(' '.join(current_row))
+    print(f'No presents for {nice_kids_left} nice kid/s.')
+    return
+
+
+def print_when_enough_presents(_nice_kids_with_given_present):
+    for current_row in neighborhood:
+        print(' '.join(current_row))
+    print(f'Good job, Santa! {_nice_kids_with_given_present} happy nice kid/s.')
+    return
+
+
+def give_cookie_presents(_santa_position, _next_santa_position, _count_presents, _nice_kids_given_presents):
+    neighborhood[_santa_position[0]][_santa_position[1]] = '-'
+    neighborhood[_next_santa_position[0]][_next_santa_position[1]] = 'S'
+
+    r = _next_santa_position[0]
+    c = _next_santa_position[1]
+
+    value_up = neighborhood[r + directions['up'][0]][c + directions['up'][1]]
+    neighborhood[r + directions['up'][0]][c + directions['up'][1]] = '-'
+    value_right = neighborhood[r + directions['right'][0]][c + directions['right'][1]]
+    neighborhood[r + directions['right'][0]][c + directions['right'][1]] = '-'
+    value_down = neighborhood[r + directions['down'][0]][c + directions['down'][1]]
+    neighborhood[r + directions['down'][0]][c + directions['down'][1]] = '-'
+    value_left = neighborhood[r + directions['left'][0]][c + directions['left'][1]]
+    neighborhood[r + directions['left'][0]][c + directions['left'][1]] = '-'
+
+    position_values = [value_up, value_right, value_down, value_left]
+    for value in position_values:
+        if value in 'X':
+            _count_presents -= 1
+        if value in 'V':
+            _nice_kids_given_presents += 1
+            _count_presents -= 1
+    return [_next_santa_position, _count_presents, _nice_kids_given_presents]
+
+
+def give_presents(_santa_position, _next_santa_position, _count_presents, _nice_kids_given_presents):
+    position_value = neighborhood[_next_santa_position[0]][_next_santa_position[1]]
+    neighborhood[_santa_position[0]][_santa_position[1]] = '-'
+    neighborhood[_next_santa_position[0]][_next_santa_position[1]] = 'S'
+    if position_value in 'XV':
+        if position_value in 'X':
+            _count_presents -= 1
+        if position_value in 'V':
+            _nice_kids_given_presents += 1
+            _count_presents -= 1
+    return [_next_santa_position, _count_presents, _nice_kids_given_presents]
+
+
+def find_next_santa_position(_direction, _santa_position):
+    old_santa_row, old_santa_col = _santa_position[0], _santa_position[1]
+    direction_row, direction_col = _direction[0], _direction[1]
+    _next_santa_position = [(old_santa_row + direction_row), (old_santa_col + direction_col)]
+    return _next_santa_position
+
+
+neighborhood = list()
+for row in range(size_matrix):
+    neighborhood_row = list(input().split())
+    neighborhood.append(neighborhood_row)
+
+for row in range(size_matrix):
+    for col in range(size_matrix):
+        current_cell = neighborhood[row][col]
         if current_cell == 'S':
             santa_position = [row, col]
-        elif current_cell == 'X':
-            naughty_kids_count += 1
         elif current_cell == 'V':
             nice_kids_count += 1
 
-# receive the commands
-commands_deque = deque()
 command = input()
-while command != 'Christmas morning':
-    commands_deque.append(command)
+global next_santa_position
+while command != 'Christmas morning' and count_presents:
+
+    if command == 'up':
+        next_santa_position = find_next_santa_position(directions['up'], santa_position)
+    elif command == 'right':
+        next_santa_position = find_next_santa_position(directions['right'], santa_position)
+    elif command == 'down':
+        next_santa_position = find_next_santa_position(directions['down'], santa_position)
+    elif command == 'left':
+        next_santa_position = find_next_santa_position(directions['left'], santa_position)
+    if neighborhood[next_santa_position[0]][next_santa_position[1]] == 'C':
+        santa_position, count_presents, nice_kids_given_presents = give_cookie_presents(
+            santa_position, next_santa_position, count_presents, nice_kids_given_presents
+        )
+    else:
+        santa_position, count_presents, nice_kids_given_presents = give_presents(
+            santa_position, next_santa_position, count_presents, nice_kids_given_presents
+        )
+    if count_presents == 0:
+        break
     command = input()
 
-
-def change_santa_position(santa_position_arg, direction):
-    new_santa_row = santa_position_arg[0] + direction[0]
-    new_santa_col = santa_position_arg[1] + direction[0]
-    return [new_santa_row, new_santa_col]
-
-
-def give_cookie_presents(position_coordinates, matrix_arg, count_of_presents_arg):
-    row = position_coordinates[0]
-    col = position_coordinates[1]
-    position_type = matrix_arg[row][col]
-    if position_type == 'X':
-        count_of_presents_arg -= 1
-        matrix_arg[row][col] = '-'
-        return [matrix_arg, count_of_presents_arg]
-    elif position_type == 'V':
-        count_of_presents_arg -= 1
-        matrix_arg[row][col] = '-'
-        return [matrix_arg, count_of_presents_arg]
-    else:
-        matrix_arg[row][col] = '-'
-        return [matrix_arg, count_of_presents_arg]
-
-
-def do_cookie_mood(santa_position_arg, matrix_arg, count_of_presents_arg):
-    position_up_coordinates = change_santa_position(santa_position_arg, move_up)
-    matrix_arg, count_of_presents_arg = give_cookie_presents(
-        position_up_coordinates, matrix_arg, count_of_presents_arg
-    )
-    if len(count_of_presents_arg) == 0:
-        return [santa_position_arg, matrix_arg, count_of_presents_arg]
-    position_down_coordinates = change_santa_position(santa_position_arg, move_down)
-    matrix_arg, count_of_presents_arg = give_cookie_presents(
-        position_down_coordinates, matrix_arg, count_of_presents_arg
-    )
-    if len(count_of_presents_arg) == 0:
-        return [santa_position_arg, matrix_arg, count_of_presents_arg]
-    position_left_coordinates = change_santa_position(santa_position_arg, move_left)
-    matrix_arg, count_of_presents_arg = give_cookie_presents(
-        position_left_coordinates, matrix_arg, count_of_presents_arg
-    )
-    if len(count_of_presents_arg) == 0:
-        return [santa_position_arg, matrix_arg, count_of_presents_arg]
-    position_right_coordinates = change_santa_position(santa_position_arg, move_right)
-    matrix_arg, count_of_presents_arg = give_cookie_presents(
-        position_right_coordinates, matrix_arg, count_of_presents_arg
-    )
-    return [santa_position_arg, matrix_arg, count_of_presents_arg]
-
-
-def give_presents(santa_position_arg, matrix_arg, count_of_presents_arg):
-    santa_row = santa_position_arg[0]
-    santa_col = santa_position_arg[1]
-    position_type = matrix_arg[santa_row][santa_col]
-    if position_type == '-':
-        return [matrix_arg, count_of_presents_arg]
-    elif position_type == 'X':
-        matrix_arg[santa_row][santa_col] = '-'
-        return [matrix_arg, count_of_presents_arg]
-    elif position_type == 'V':
-        matrix_arg[santa_row][santa_col] = '-'
-        count_of_presents_arg -= 1
-        return [matrix_arg, count_of_presents_arg]
-    elif position_type == 'C':
-        do_cookie_mood(santa_position_arg, matrix_arg, count_of_presents_arg)
-        return [matrix_arg, count_of_presents_arg]
-
-
-def print_output(matrix_arg, nice_kids_count_arg):
-    pass
-
-
-while len(commands_deque) > 0:
-    current_command = commands_deque.popleft()
-    if current_command == 'up':
-        santa_position = change_santa_position(santa_position, move_up)
-        matrix, count_of_presents = give_presents(santa_position, matrix, count_of_presents)
-        if len(count_of_presents) == 0:
-            print_output(matrix, nice_kids_count)
-    elif current_command == 'down':
-        santa_position = change_santa_position(santa_position, move_down)
-        matrix, count_of_presents = give_presents(santa_position, matrix, count_of_presents)
-    elif current_command == 'left':
-        santa_position = change_santa_position(santa_position, move_left)
-        matrix, count_of_presents = give_presents(santa_position, matrix, count_of_presents)
-    elif current_command == 'right':
-        santa_position = change_santa_position(santa_position, move_right)
-        matrix, count_of_presents = give_presents(santa_position, matrix, count_of_presents)
+if count_presents == 0 and nice_kids_count > nice_kids_given_presents:
+    print_when_not_enough_presents(nice_kids_given_presents)
+else:
+    print_when_enough_presents(nice_kids_given_presents)
