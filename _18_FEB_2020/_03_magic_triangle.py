7a902187199bcd8bd093e3c2a0eb8f26fb8de6d6
Andrey-V-Georgiev <andrey.v.georgiev@gmail.com>
2020-06-14 22:01:27 +0300

    _18_FEB_2020/_03_magic_triangle(100%)

---
+++ _18_FEB_2020/_03_magic_triangle.py
@@ -0,0 +1,31 @@
+def get_magic_triangle(n):
+    triangle_list = [[1], [1, 1]]
+    for i in range(1, n - 1):
+        current_row = triangle_list[i]
+        next_row = list()
+        for j in range(len(current_row) - 1):
+            next_row.append(current_row[j] + current_row[j + 1])
+        next_row.insert(0, 1)
+        next_row.append(1)
+        triangle_list.append(next_row)
+    return triangle_list
+
+
+print(get_magic_triangle(5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
